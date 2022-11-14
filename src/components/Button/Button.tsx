c4b7d316cca158b1e13fd3738a99af9a6406f7b5
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-11-14 20:03:42 +0700

    feat: component button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,8 +4,7 @@ import styles from './Button.module.css';
 const Button: React.FC<any> = ({title, onSubmitHandler, onClose}) => {
     return (
         <div className={styles.button}>
-            {title === 'Simpan' && <button type="button" className={styles.primary}  onClick={onSubmitHandler}>{title}</button>}
-            {title === 'Batal' && <button type="button" className={styles.secondary} onClick={onClose}>{title}</button>}
+            <button type="button" className={`${title === 'Simpan' ? `${styles.primary}` : `${styles.secondary}`}`}  onClick={title === 'Simpan' ? onSubmitHandler : onClose}>{title}</button>
         </div>
     )
 }
